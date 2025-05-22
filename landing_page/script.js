25acd0a6e2893d60337486fa35b701859c1b18b1
levys <levy.jardim02@gmail.com>
2025-05-22 09:46:22 -0300

    deix so uma pasta

--- landing_page/script.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('form-campeonato').addEventListener('submit', function(e) {
-  e.preventDefault();
-  const mensagem = document.getElementById('mensagem');
-  mensagem.textContent = 'Campeonato cadastrado com sucesso!';
-  mensagem.style.color = 'green';
-  mensagem.style.fontWeight = 'bold';
-  mensagem.style.animation = 'fadeIn 1s ease-in';
-  this.reset();
-});
